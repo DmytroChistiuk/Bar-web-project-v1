8dfc1ec0ba8ca24f3a40dfe56a7573f93a1fe985
nldRDMVk <dmytro.chystiuk@nure.ua>
2021-06-19 21:25:39 +0300

    cjm

--- Task4/src/com/company/Part1.java
+++ Task4/src/com/company/Part1.java
@@ -8,35 +8,39 @@ public class Part1 {
 
 
     public static void main(String[] args) throws IOException {
-        HashMap<String,Integer> domenMap = new HashMap<>();
-       String[] arrayDomens = ArrayOfDomenFromFile().toArray(new String[0]);
-       HashMap<String,Integer> additionalMap = new HashMap<>();
-        for (int i = 0; i < arrayDomens.length; i++) {
+        int itarator=0;
+        HashMap<String,Integer> domenMap = new HashMap<>(); // мапа результирующих доменов
+       String[] arrayDomens = ArrayOfDomenFromFile().toArray(new String[0]); // преобразуем лист в массив стрингов для удобства
+       HashMap<String,Integer> additionalMap = new HashMap<>(); // дополнительная мапа для переноса элемемнтов в результирующую мапу
+        for (int i = 0; i < arrayDomens.length; i++) {  // записываем в доп. мапу все домены
             additionalMap.put(arrayDomens[i],1);
         }
-        int itarator=0;
-        for (int i = 0; i < arrayDomens.length; i++) {
-            for (Map.Entry<String,Integer> entry:additionalMap.entrySet()){
-                if (arrayDomens[i].equals(entry.getKey()))
+
+        for (int i = 0; i < arrayDomens.length; i++) { // проходимся по масиву доменов
+            for (Map.Entry<String,Integer> entry:additionalMap.entrySet()){ // проходимся по мапе
+                if (arrayDomens[i].equals(entry.getKey())) // сравниваем елемент масива и мапы
                 {
                    itarator = entry.getValue();
                    itarator++;
-                    domenMap.remove(arrayDomens[i]);
+                    domenMap.remove(arrayDomens[i]); // удаляем прежний элемент ключ - значение,чтобы изменить ключ
                     domenMap.put(arrayDomens[i],itarator);
                 }
                 else {
-                    domenMap.put(arrayDomens[i],1);
+                    domenMap.put(arrayDomens[i],1); // добавляем елемент, он встречается первый раз
                 }
 
             }
         }
-        for (Map.Entry<String,Integer> entry:domenMap.entrySet()){
+        for (Map.Entry<String,Integer> entry:domenMap.entrySet()){ // выводим в консоль все домены и их частоту встречаемости
         System.out.println("Сколько раз встречается ["+entry.getValue() + "] ~~ Что встречается ["+ entry.getKey()+"]");
         }
     }
 
 
     public static List<String> ArrayOfDomenFromFile() throws IOException {
+        // функция считывания с файла url
+        // делим url по "/" получаем домены
+        // записываем домены в Лист
         FileReader fileReader = new FileReader("urls.txt");
         Scanner scanner = new Scanner(fileReader);
         List<String> arrayDomens =new ArrayList<>();
